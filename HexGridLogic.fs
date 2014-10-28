615b0459a51ff5df0ef3b1945016f71b5da6ede6
maxenko <maxostap@gmail.com>
2014-10-28 08:19:10 -0700

    populate Neighbors property

--- HexGridLogic.fs
+++ HexGridLogic.fs
@@ -14,6 +14,7 @@ type HexVector =
     static member W     = HexVector.mask.[1]
     static member SW    = HexVector.mask.[2]
     static member SE    = HexVector.mask.[5]
+
     static member GetVector (d:Direction) =
         match d with
         | NE when d = Direction.NE -> HexVector.NE
@@ -23,9 +24,10 @@ type HexVector =
         | SE when d = Direction.SE -> HexVector.SE
         | SW when d = Direction.SW -> HexVector.SW
         | _ -> HexVector.E
+
     static member GetNeighborCoordinates(x,y,z,d) =
         let v = HexVector.GetVector(d)
-        [|x+v.[0],y+v.[1],z+v.[2]|] 
+        [|x+v.[0];y+v.[1];z+v.[2]|] 
 
 type HexCellNeighbors<'n>(cell:'n) =
     let parent = cell
@@ -36,9 +38,9 @@ type HexCellNeighbors<'n>(cell:'n) =
     member val SE   = cell with get, set
     member val SW   = cell with get, set
       
-type HexCellCoord(hexX : int, hexY : int, hexZ : int, ?x : single, ?y : single) as self =
+type HexCellCoord(hexX : int, hexY : int, hexZ : int, ?x : single, ?y : single) =
 
-    static member Nothing = new HexCellCoord(-1,-1,-1,100000.f,1000000.f)
+    static member Nothing = new HexCellCoord(-1,-1,-1,100000.f,1000000.f) // placeholder, representing non-location
     
     // add to collection
     member val HexX = hexX with get
@@ -51,25 +53,40 @@ type HexCellCoord(hexX : int, hexY : int, hexZ : int, ?x : single, ?y : single) 
 
 type HexGridMap( radius : int, cellSize : single) =
 
-    member x.MakeCells() =
-        (*
-        let mapNeighbors(coords:HexCellCoord[]) =
-            let exists d cell =
-                HexVector.GetVector(d) // add custom operator to hexvector to perform addition
-            let assignNeighbors (c:HexCellCoord) =
-                Seq.iter  
-                     
-            Seq.iter (fun c -> assignNeighbors ) coords
-            coords       *)
+    let mutable cells = Array.empty<HexCellCoord>
+    member x.Cells with get () = cells and set (v) = cells <- v
 
-        [   for x in -radius .. radius do
-                for z in -radius .. radius do
-                    // use axial coordinate system to translate to xy space
-                    let r = (single)z
-                    let q = (single)x
-                    let x2d = cellSize - 3.f/2.f * q
-                    let y2d = cellSize * sqrt((*3.f*)1.f) * (r + q/2.f)
-                    let cubeGridY = -x-z
-                    if (abs(cubeGridY) <= radius ) then // keep overall shape of the grid hexagonal
-                        yield new HexCellCoord(x,cubeGridY,z,x2d,y2d)
-        ] |> Seq.toArray //|> mapNeighbors 
+    member x.MakeCells() =
+        let cells = 
+            [   for x in -radius .. radius do
+                    for z in -radius .. radius do
+                        // use axial coordinate system to translate to xy space
+                        let r = (single)z
+                        let q = (single)x
+                        let x2d = cellSize - 3.f/2.f * q
+                        let y2d = cellSize * sqrt((*3.f*)1.f) * (r + q/2.f)
+                        let cubeGridY = -x-z
+                        if (abs(cubeGridY) <= radius ) then // keep overall shape of the grid hexagonal
+                            let cell = new HexCellCoord(x,cubeGridY,z,x2d,y2d)
+                            yield cell
+            ] |> Seq.toArray //|> mapNeighbors
+        
+        // map out neighbor coordinates
+        let assignCoords (c:HexCellCoord) =
+            let getC d =
+                let v = HexVector.GetNeighborCoordinates(c.HexX,c.HexY,c.HexZ,d)
+                new HexCellCoord(v.[0],v.[1],v.[2])
+            
+            c.Neighbors.E   <- getC Direction.E
+            c.Neighbors.NE  <- getC Direction.NE
+            c.Neighbors.NW  <- getC Direction.NW
+            c.Neighbors.SE  <- getC Direction.SE
+            c.Neighbors.SW  <- getC Direction.SW
+            c.Neighbors.W   <- getC Direction.W
+
+        Seq.iter( fun (c : HexCellCoord) ->
+            assignCoords c
+        ) cells    
+         
+        x.Cells <- cells
+        cells
